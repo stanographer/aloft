b3d8947de4a766ba9afa46a16cb9a2a44c0c8a90
stanographer <sakai.stanley@gmail.com>
2018-11-19 17:09:33 -0500

    Added Express testing.

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,22 +1,37 @@
 process.env.NODE_ENV = 'test';
 
 const request = require('supertest');
+const WebSocket = require('ws');
 
-describe('Loading Express', () => {
+describe('Basic Express/backend Functionality Tests', () => {
   let app;
 
   beforeEach(() => {
     app = require('../app');
   });
 
-  it('responds to /', function testSlash(done) {
+  it('Responds to /.', done => {
     request(app)
       .get('/')
       .expect(200, done);
   });
-  it('404s everything else', function testPath(done) {
+
+  it('404s everything else.', done => {
     request(app)
       .get('/foo/baz/bar')
       .expect(404, done);
+  });
+
+  it('WebSocket responds to a \'ping\' with a \'pong.\'', done => {
+    const socket = new WebSocket('http://localhost:5000', '', '');
+
+    socket.on('connected', () => console.log('Websocket connected.'));
+    socket.emit('ping', ' ');
+    socket.on('pong', data => {
+      console.log(`Data received from Websocket: ${data}.`);
+    });
+
+    done();
+
   });
 });
