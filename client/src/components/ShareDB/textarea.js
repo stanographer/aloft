0907905238bbc8c2d3fe433cd5b61beb217ea069
stanographer <sakai.stanley@gmail.com>
2018-12-14 16:40:37 -0500

    Removed unused code.

--- client/src/components/ShareDB/textarea.js
+++ client/src/components/ShareDB/textarea.js
@@ -217,25 +217,3 @@ export const attachTextarea = function (elem, doc) {
   return doc;
 };
 
-// (() => {
-//   let el = document.getElementById('sharedTextArea');
-//   el.onkeydown = (e) => {
-//     if (e.keyCode === 9) { // tab was pressed
-//
-//       // Get caret position/selection
-//       let val = this.value,
-//         start = this.selectionStart,
-//         end = this.selectionEnd;
-//
-//       // set textarea value to: text before caret + tab + text after caret
-//       this.value = val.substring(0, start) + '\t' + val.substring(end);
-//
-//       // put caret at right position again
-//       this.selectionStart = this.selectionEnd = start + 1;
-//
-//       // prevent the focus lose
-//       return false;
-//     }
-//   };
-// })();
-
